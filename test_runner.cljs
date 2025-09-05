2fdf3992ad7f457e5fef93be77139acd4702fc47
Casey Link <casey@outskirtslabs.com>
2025-09-05 08:57:11 +0200

    wip

--- test_runner.cljs
+++
@@ -1,8 +0,0 @@
-(ns test-runner
-  (:require [cljs.test :refer [run-tests]]
-            [ol.rfc9839-cljs-test]))
-
-(defn -main []
-  (run-tests 'ol.rfc9839-cljs-test))
-
-(set! *main-cli-fn* -main)
