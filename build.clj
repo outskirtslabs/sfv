30b95daea8f8db6c3385ade11c0282601f06a84f
Casey Link <casey@outskirtslabs.com>
2026-06-11 11:31:45 +0200

    build packages with clojure nix locker

--- build.clj
+++ build.clj
@@ -3,25 +3,48 @@
 (ns build
   (:require [clojure.tools.build.api :as b]
             [clojure.edn :as edn]
+            [clojure.java.io :as io]
             [clojure.string :as str]))
 
 (def project (-> (edn/read-string (slurp "deps.edn")) :aliases :neil :project))
-(def rev (str/trim (b/git-process {:git-args "rev-parse HEAD"})))
 (def lib (:name project))
 (def version (:version project))
 (def license-id (-> project :license :id))
 (def license-file (or (-> project :license :file) "LICENSE"))
 (def description (:description project))
-(def repo-url-prefix (:url project (-> (b/git-process {:git-args "remote get-url origin"})
-                                       (str/trim)
-                                       (str/replace #"\.git$" ""))))
+
+(defn- git-origin-url []
+  (try
+    (some-> (b/git-process {:git-args "remote get-url origin"})
+            str/trim
+            (str/replace #"\.git$" ""))
+    (catch Exception _
+      nil)))
+
+(defn- git-rev []
+  (or (some-> (System/getenv "GIT_REV")
+              str/trim
+              not-empty)
+      (some-> (b/git-process {:git-args "rev-parse HEAD"})
+              str/trim
+              not-empty)))
+
+(def rev (git-rev))
+(def repo-url-prefix (or (:url project) (git-origin-url)))
 (assert lib ":name must be set in deps.edn under the :neil alias")
 (assert version ":version must be set in deps.edn under the :neil alias")
 (assert description ":description must be set in deps.edn under the :neil alias")
 (assert license-id "[:license :id] must be set in deps.edn under the :neil alias")
+(assert rev "Either GIT_REV must be set or git rev-parse HEAD must succeed")
+(assert repo-url-prefix "Either :url must be set in deps.edn under the :neil alias or git remote origin must exist")
 (def class-dir "target/classes")
 (def basis_ (delay (b/create-basis {:project "deps.edn"})))
 (def jar-file (format "target/%s-%s.jar" (name lib) version))
+
+(defn- existing-paths [paths]
+  (->> paths
+       (filter #(.exists (io/file %)))
+       vec))
 
 (defn permalink [subpath]
   (str repo-url-prefix "/blob/" rev "/" subpath))
@@ -38,36 +61,36 @@
 
 (defn jar [_]
   (b/write-pom {:class-dir class-dir
-                :lib lib
-                :version version
-                :basis @basis_
-                :src-dirs ["src"]
-                :pom-data [[:description description]
-                           [:url repo-url-prefix]
-                           [:licenses
-                            [:license
-                             [:name license-id]
-                             [:url (permalink license-file)]]]
-                           (conj (url->scm repo-url-prefix) [:tag rev])]})
+                :lib       lib
+                :version   version
+                :basis     @basis_
+                :src-dirs  (existing-paths ["src"])
+                :pom-data  [[:description description]
+                            [:url repo-url-prefix]
+                            [:licenses
+                             [:license
+                              [:name license-id]
+                              [:url (permalink license-file)]]]
+                            (conj (url->scm repo-url-prefix) [:tag rev])]})
 
-  (b/copy-dir {:src-dirs ["src" "resources"]
+  (b/copy-dir {:src-dirs   (existing-paths ["src" "resources"])
                :target-dir class-dir})
   (b/jar {:class-dir class-dir
-          :jar-file jar-file}))
+          :jar-file  jar-file}))
 
 (defn install [_]
   (jar {})
-  (b/install {:basis @basis_
-              :lib lib
-              :version version
-              :jar-file jar-file
+  (b/install {:basis     @basis_
+              :lib       lib
+              :version   version
+              :jar-file  jar-file
               :class-dir class-dir}))
 
 (defn deploy [opts]
   (jar opts)
   ((requiring-resolve 'deps-deploy.deps-deploy/deploy)
    (merge {:installer :remote
-           :artifact jar-file
-           :pom-file (b/pom-path {:lib lib :class-dir class-dir})}
+           :artifact  jar-file
+           :pom-file  (b/pom-path {:lib lib :class-dir class-dir})}
           opts))
   opts)
--- flake.nix
+++ flake.nix
@@ -1,36 +1,164 @@
 {
   description = "dev env";
+
   inputs = {
     nixpkgs.url = "https://flakehub.com/f/NixOS/nixpkgs/0.1"; # tracks nixpkgs unstable branch
-    flakelight.url = "github:nix-community/flakelight";
-    flakelight.inputs.nixpkgs.follows = "nixpkgs";
+    devshell.url = "github:numtide/devshell";
+    devshell.inputs.nixpkgs.follows = "nixpkgs";
+    devenv.url = "github:ramblurr/nix-devenv";
+    devenv.inputs.nixpkgs.follows = "nixpkgs";
   };
+
   outputs =
-    {
+    inputs@{
       self,
-      flakelight,
+      devenv,
+      devshell,
       ...
     }:
-    flakelight ./. {
+    let
+      jdk = "jdk21";
+      projectSrc =
+        pkgs:
+        let
+          root = toString ./.;
+        in
+        pkgs.lib.cleanSourceWith {
+          src = ./.;
+          filter =
+            path: _type:
+            let
+              rel = pkgs.lib.removePrefix (root + "/") (toString path);
+              base = builtins.baseNameOf path;
+            in
+            !(
+              base == ".git"
+              || rel == "result"
+              || rel == ".nrepl-port"
+              || rel == ".clojure-mcp"
+              || pkgs.lib.hasPrefix ".clj-kondo/" rel
+              || pkgs.lib.hasPrefix ".cpcache/" rel
+              || pkgs.lib.hasPrefix ".direnv/" rel
+              || pkgs.lib.hasPrefix ".lsp/" rel
+              || pkgs.lib.hasPrefix "example-project/" rel
+              || pkgs.lib.hasPrefix "extra/" rel
+              || pkgs.lib.hasPrefix "target/" rel
+            );
+        };
+    in
+    devenv.lib.mkFlake ./. {
+      inherit inputs;
+
+      withOverlays = [
+        devshell.overlays.default
+        devenv.overlays.default
+      ];
+
+      packages = {
+        default =
+          pkgs:
+          let
+            jdkPackage = pkgs.${jdk};
+            clojure = pkgs.clojure.override { jdk = jdkPackage; };
+            src = projectSrc pkgs;
+            gitRev =
+              if self ? rev then
+                self.rev
+              else if self ? dirtyRev then
+                self.dirtyRev
+              else
+                "dirty";
+            clojureLocker = devenv.clojure.mkLockfile {
+              inherit pkgs src;
+              jdk = jdkPackage;
+              lockfile = "./deps-lock.json";
+              extraPrepInputs = [ pkgs.git ];
+            };
+          in
+          pkgs.stdenv.mkDerivation {
+            pname = "sfv";
+            version = "0.1.0";
+            inherit src;
+            nativeBuildInputs = [
+              clojure
+              pkgs.coreutils
+              pkgs.findutils
+              pkgs.git
+              jdkPackage
+            ];
+            GIT_REV = gitRev;
+            JAVA_HOME = jdkPackage.home;
+            buildPhase = ''
+              runHook preBuild
+
+              source ${clojureLocker.shellEnv}
+              export JAVA_HOME="${jdkPackage.home}"
+              export JAVA_CMD="${jdkPackage}/bin/java"
+              export GITLIBS="$HOME/.gitlibs"
+              export JAVA_TOOL_OPTIONS="$JAVA_TOOL_OPTIONS -Djava.io.tmpdir=$TMPDIR"
+
+              clojure -Srepro -X:deps prep :aliases '[:kaocha :build]'
+              clojure -Srepro -M:kaocha
+              clojure -Srepro -T:build jar
+
+              runHook postBuild
+            '';
+            installPhase = ''
+              runHook preInstall
+
+              mkdir -p "$out"
+              cp "$(find target -type f -name '*.jar' -print | head -n 1)" "$out/"
+
+              runHook postInstall
+            '';
+          };
+
+        locker =
+          pkgs:
+          let
+            jdkPackage = pkgs.${jdk};
+            clojure = pkgs.clojure.override { jdk = jdkPackage; };
+            src = projectSrc pkgs;
+            clojureLocker = devenv.clojure.mkLockfile {
+              inherit pkgs src;
+              jdk = jdkPackage;
+              lockfile = "./deps-lock.json";
+              extraPrepInputs = [ pkgs.git ];
+            };
+          in
+          clojureLocker.commandLocker ''
+            export HOME="$tmp/home"
+            export GITLIBS="$HOME/.gitlibs"
+            unset CLJ_CACHE CLJ_CONFIG XDG_CACHE_HOME XDG_CONFIG_HOME XDG_DATA_HOME
+            export GIT_REV="lockfile-generation"
+
+            ${clojure}/bin/clojure -Srepro -X:deps prep :aliases "[:kaocha :build]"
+            ${clojure}/bin/clojure -Srepro -P -M:kaocha
+            ${clojure}/bin/clojure -Srepro -T:build jar
+          '';
+      };
 
       devShell =
         pkgs:
         let
-          javaVersion = "24";
-          jdk = pkgs."jdk${javaVersion}";
-          clojure = pkgs.clojure.override { inherit jdk; };
-          libraries = [ ];
+          jdkPackage = pkgs.${jdk};
+          clojure = pkgs.clojure.override { jdk = jdkPackage; };
         in
-        {
+        pkgs.devshell.mkShell {
+          imports = [
+            devenv.capsules.base
+          ];
           packages = [
+            self.packages.${pkgs.system}.locker
             clojure
-            jdk
+            jdkPackage
             pkgs.dumbpipe
             pkgs.clojure-lsp
             pkgs.clj-kondo
             pkgs.cljfmt
             pkgs.babashka
             pkgs.git
+            pkgs.wget
             (pkgs.writeScriptBin "run-clojure-mcp" ''
               #!/usr/bin/env bash
                 set -euo pipefail
@@ -42,8 +170,7 @@
                 ${clojure}/bin/clojure -X:mcp/clojure :port $PORT
             '')
           ];
-          env.LD_LIBRARY_PATH = pkgs.lib.makeLibraryPath libraries;
-          shellHook = ''
+          devshell.startup.fetch-extra.text = ''
             mkdir -p extra/
             pushd extra/
             test -f rfc9651.txt || wget -q https://www.rfc-editor.org/rfc/rfc9651.txt
@@ -52,10 +179,11 @@
           '';
         };
 
-      flakelight.builtinFormatters = false;
-      formatters = pkgs: {
-        "*.nix" = "${pkgs.nixfmt}/bin/nixfmt";
-        "*.clj" = "${pkgs.cljfmt}/bin/cljfmt fix";
+      treefmtConfig = {
+        programs = {
+          nixfmt.enable = true;
+          cljfmt.enable = true;
+        };
       };
     };
 }
